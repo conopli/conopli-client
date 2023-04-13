a276c6d0e9977f8f4afd04d88929a50684cc8a0e
nine_floor <less0805@gmail.com>
2023-04-13 22:48:34 +0900

    design: 스타일을 위한 색상 프리셋 생성
    Co-authored-by: Jade <ansmeer008@users.noreply.github.com>

---
+++ theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  lime: '#DCF235',
+  lightGray: '#DBDBDB',
+  black: '#0D0D0D',
+  background: '#1D1D1D',
+  white: '#FAFAFA',
+  gray: '#666666',
+  lightBlue: '#B1B1DF',
+  kakao: '#FEE500',
+  naver: '#03C75A',
+  violet: '#EEE5FF',
+  red: '#BF2A2A',
+};
