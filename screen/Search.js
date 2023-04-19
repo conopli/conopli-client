e970598f546fae238d37b30fe2a18c0ba79b2379
nine_floor <less0805@gmail.com>
2023-04-19 19:47:34 +0900

    feat: Search 스크린 마크업

--- screen/Search.js
+++ screen/Search.js
@@ -1,10 +1,55 @@
-import { Text, View, Platform } from 'react-native';
-import { theme } from '../theme';
+import { View, TextInput, ScrollView } from 'react-native';
+import styles from './Search.style';
+import DropDownPicker from 'react-native-dropdown-picker';
+import { useState } from 'react';
+import SearchButton from '../components/SearchButton';
+import MusicItem from '../components/MusicItem';
 
 const Search = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('kpop');
+  const [items, setItems] = useState([
+    { label: '한국', value: 'kpop' },
+    { label: '영어', value: 'pop' },
+    { label: '일본', value: 'jpop' },
+  ]);
+
+  //! 후에 더미데이터로 작업 시 FlatList 사용할 것
   return (
-    <View style={{ backgroundColor: theme.background, flex: 1 }}>
-      <Text>검색</Text>
+    <View style={styles.container}>
+      <View style={styles.dropdown}>
+        <DropDownPicker
+          style={styles.picker}
+          containerStyle={{ width: 'auto' }}
+          textStyle={{ fontSize: 16, fontWeight: 'bold' }}
+          arrowIconContainerStyle={{ marginLeft: 4 }}
+          tickIconContainerStyle={{ marginLeft: 4 }}
+          placeholder={value}
+          open={open}
+          value={value}
+          items={items}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setItems}
+        />
+      </View>
+      <View style={styles.search}>
+        <SearchButton />
+        <TextInput style={styles.input} />
+      </View>
+      <ScrollView style={styles.resultList}>
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+        <MusicItem />
+      </ScrollView>
     </View>
   );
 };
--- screen/Search.style.js
+++ screen/Search.style.js
@@ -4,9 +4,35 @@ import { theme } from '../theme';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: theme.background,
+    paddingVertical: 24,
+    paddingHorizontal: 16,
+  },
+  dropdown: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    zIndex: 1,
+  },
+  picker: {
+    borderRadius: 4,
+    marginBottom: 12,
+    paddingVertical: 8,
+    minHeight: 'auto',
+    width: 'auto',
     justifyContent: 'center',
+  },
+  search: {
+    flexDirection: 'row',
+  },
+  input: {
+    backgroundColor: theme.gray,
+    flex: 1,
+    borderRadius: 4,
+    marginLeft: 8,
+  },
+  resultList: {
+    flex: 1,
+    marginTop: 24,
   },
 });
 
