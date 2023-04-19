be40ea9d9b4b4af1aaafba697e05fc651248bbb6
ansmeer008 <ansmeer008@gmail.com>
2023-04-19 23:14:01 +0900

    feat: Popular 페이지에 버튼 클릭 로직 추가

--- screen/Populer.js
+++ screen/Populer.js
@@ -1,26 +1,50 @@
 import { Text, View } from 'react-native';
-import { theme } from '../theme';
 import SmallButton from '../components/SmallButton';
 import chart_dummy from '../assets/chart_dummy';
 import ListItem from '../components/popular/ListItem';
 import styles from './Populer.style.js';
+import { ScrollView } from 'react-native';
+import { useState } from 'react';
 
 const Populer = () => {
+  //임시로 만든 상태라 조잡함. 이후 수정할 수 있으면 수정 필요.
+  const [clicked, setClicked] = useState([false, false, false]);
+
+  const clickHandelr = (e) => {
+    console.log(e.nativeEvent.target);
+    if (e.nativeEvent.target === 1409) {
+      setClicked([true, false, false]);
+    } else if (e.nativeEvent.target === 1417) {
+      setClicked([false, true, false]);
+    } else if (e.nativeEvent.target === 1425) {
+      setClicked([false, false, true]);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.buttonBox}>
-        <SmallButton text="가요" />
+        <SmallButton
+          text="가요"
+          isClicked={clicked[0]}
+          setIsClicked={clickHandelr}
+        />
         <View style={styles.abroad}>
-          <SmallButton text="POP" style={styles.pop} />
-          <SmallButton text="J-POP" />
+          <SmallButton
+            text="POP"
+            isClicked={clicked[1]}
+            setIsClicked={clickHandelr}
+          />
+          <SmallButton
+            text="J-POP"
+            isClicked={clicked[2]}
+            setIsClicked={clickHandelr}
+          />
         </View>
       </View>
-      <View
-        style={{
-          flex: 9,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
+      <ScrollView
+        style={styles.listContainer}
+        contentContainerStyle={{ rowGap: 8 }}
       >
         <ListItem
           rate={chart_dummy[0].rate}
@@ -76,7 +100,49 @@ const Populer = () => {
           singer={chart_dummy[8].singer}
           number={chart_dummy[8].cono_number}
         />
-      </View>
+        <ListItem
+          rate={chart_dummy[9].rate}
+          title={chart_dummy[9].title}
+          singer={chart_dummy[9].singer}
+          number={chart_dummy[9].cono_number}
+        />
+        <ListItem
+          rate={chart_dummy[10].rate}
+          title={chart_dummy[10].title}
+          singer={chart_dummy[10].singer}
+          number={chart_dummy[10].cono_number}
+        />
+        <ListItem
+          rate={chart_dummy[10].rate}
+          title={chart_dummy[10].title}
+          singer={chart_dummy[10].singer}
+          number={chart_dummy[10].cono_number}
+        />
+        <ListItem
+          rate={chart_dummy[10].rate}
+          title={chart_dummy[10].title}
+          singer={chart_dummy[10].singer}
+          number={chart_dummy[10].cono_number}
+        />
+        <ListItem
+          rate={chart_dummy[10].rate}
+          title={chart_dummy[10].title}
+          singer={chart_dummy[10].singer}
+          number={chart_dummy[10].cono_number}
+        />
+        <ListItem
+          rate={chart_dummy[10].rate}
+          title={chart_dummy[10].title}
+          singer={chart_dummy[10].singer}
+          number={chart_dummy[10].cono_number}
+        />
+        <ListItem
+          rate={chart_dummy[10].rate}
+          title={chart_dummy[10].title}
+          singer={chart_dummy[10].singer}
+          number={chart_dummy[10].cono_number}
+        />
+      </ScrollView>
     </View>
   );
 };
