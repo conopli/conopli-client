087dd6c50e01bae094b6b748f99af29ad5e6024d
ansmeer008 <ansmeer008@gmail.com>
2023-04-23 21:43:57 +0900

    feat: Login Page 컴포넌트

---
+++ screen/Login.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, View, Platform } from 'react-native';
+import styles from './Login.style.js';
+import AuthButton from '../components/login/AuthButton.js';
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.login_text}>
+        해당 기능은 로그인 후 이용 가능합니다.
+      </Text>
+      <View style={styles.button_box}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            height: 40,
+          }}
+        >
+          <AuthButton type="kakao" />
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            height: 40,
+          }}
+        >
+          <AuthButton type="google" />
+        </View>
+        <View style={{ flex: 1, flexDirection: 'row', height: 40 }}>
+          <AuthButton type="naver" />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Login;
