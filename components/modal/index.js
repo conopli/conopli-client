d3618c6bc2dac1b4a54f5980de3b2989d3555294
Jade <neederjade@gmail.com>
2023-05-01 15:49:04 +0900

    feat: index.js에 DeleteModal 추가

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,4 +1,5 @@
 import AddSongModal from './AddSongModal.js';
 import ConfirmModal from './ConfirmModal.js';
+import DeleteModal from './DeleteModal.js';
 
-export { AddSongModal, ConfirmModal };
+export { AddSongModal, ConfirmModal, DeleteModal };
