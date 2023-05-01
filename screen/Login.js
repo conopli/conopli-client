8a56adc438d5a20468bf429e81cc45239d107052
nine_floor <less0805@gmail.com>
2023-05-01 16:30:26 +0900

    fix: 폴더명 수정으로 인한 경로 수정

--- screen/Login.js
+++ screen/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, Platform } from 'react-native';
 import styles from './Login.style.js';
-import AuthButton from '../components/login/AuthButton.js';
+import AuthButton from '../components/Login/AuthButton.js';
 
 const Login = () => {
   return (
--- screen/Populer.js
+++ screen/Populer.js
@@ -1,9 +1,9 @@
 import SmallButton from '../components/SmallButton';
-import chart_dummy from '../assets/chart_dummy';
-import ListItem from '../components/popular/ListItem';
+import ListItem from '../components/Popular/ListItem';
 import styles from './Populer.style.js';
-import { Text, View, ScrollView, FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { useState } from 'react';
+import { populerDummy } from '../util/dummyData';
 
 const Populer = () => {
   //TODO::
@@ -45,107 +45,12 @@ const Populer = () => {
           />
         </View>
       </View>
-      <ScrollView
+      <FlatList
         style={styles.listContainer}
         contentContainerStyle={{ rowGap: 8 }}
-      >
-        <ListItem
-          rate={chart_dummy[0].rate}
-          title={chart_dummy[0].title}
-          singer={chart_dummy[0].singer}
-          number={chart_dummy[0].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[1].rate}
-          title={chart_dummy[1].title}
-          singer={chart_dummy[1].singer}
-          number={chart_dummy[1].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[2].rate}
-          title={chart_dummy[2].title}
-          singer={chart_dummy[2].singer}
-          number={chart_dummy[2].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[3].rate}
-          title={chart_dummy[3].title}
-          singer={chart_dummy[3].singer}
-          number={chart_dummy[3].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[4].rate}
-          title={chart_dummy[4].title}
-          singer={chart_dummy[4].singer}
-          number={chart_dummy[4].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[5].rate}
-          title={chart_dummy[5].title}
-          singer={chart_dummy[5].singer}
-          number={chart_dummy[5].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[6].rate}
-          title={chart_dummy[6].title}
-          singer={chart_dummy[6].singer}
-          number={chart_dummy[6].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[7].rate}
-          title={chart_dummy[7].title}
-          singer={chart_dummy[7].singer}
-          number={chart_dummy[7].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[8].rate}
-          title={chart_dummy[8].title}
-          singer={chart_dummy[8].singer}
-          number={chart_dummy[8].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[9].rate}
-          title={chart_dummy[9].title}
-          singer={chart_dummy[9].singer}
-          number={chart_dummy[9].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[10].rate}
-          title={chart_dummy[10].title}
-          singer={chart_dummy[10].singer}
-          number={chart_dummy[10].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[10].rate}
-          title={chart_dummy[10].title}
-          singer={chart_dummy[10].singer}
-          number={chart_dummy[10].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[10].rate}
-          title={chart_dummy[10].title}
-          singer={chart_dummy[10].singer}
-          number={chart_dummy[10].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[10].rate}
-          title={chart_dummy[10].title}
-          singer={chart_dummy[10].singer}
-          number={chart_dummy[10].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[10].rate}
-          title={chart_dummy[10].title}
-          singer={chart_dummy[10].singer}
-          number={chart_dummy[10].cono_number}
-        />
-        <ListItem
-          rate={chart_dummy[10].rate}
-          title={chart_dummy[10].title}
-          singer={chart_dummy[10].singer}
-          number={chart_dummy[10].cono_number}
-        />
-      </ScrollView>
+        data={populerDummy.data}
+        renderItem={ListItem}
+      />
     </View>
   );
 };
