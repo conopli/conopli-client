68189b1bb5256c0554ac3773c8351e7679a4b147
Jade <neederjade@gmail.com>
2023-05-01 18:49:44 +0900

    feat: EditPlaylistModal 컴포넌트 마크업

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,5 +1,6 @@
 import AddSongModal from './AddSongModal.js';
 import ConfirmModal from './ConfirmModal.js';
 import DeleteModal from './DeleteModal.js';
+import EditPlaylistModal from './EditPlaylistModal.js';
 
-export { AddSongModal, ConfirmModal, DeleteModal };
+export { AddSongModal, ConfirmModal, DeleteModal, EditPlaylistModal };
