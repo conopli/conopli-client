7d76bfde56a0ebe06dd22bc21d936df21fae2d70
nine_floor <less0805@gmail.com>
2023-05-01 19:27:31 +0900

    feat: 더미데이터 연결 및 버튼 작동 방식 변경

--- screen/Populer.js
+++ screen/Populer.js
@@ -11,37 +11,32 @@ const Populer = () => {
   //ex: {"changedTouches": [[Circular]], "identifier": 1, "locationX": 14.5, "locationY": 3, "pageX": 350.5, "pageY": 111, "target": 23, "timestamp": 2108568203.7747502, "touches": []}
   //새로운 이벤트 객체 속성을 지정해줄 수도 없음 (name과 같은)
   //세 버튼 중 한 버튼만 눌린 상태로 보여지도록 해야하는데 아래와 같은 로직으로 불가능
-  const [clicked, setClicked] = useState([false, false, false]);
-
-  const clickHandelr = (e) => {
-    console.log(e.nativeEvent.target);
-    if (e.nativeEvent.target === 5) {
-      setClicked([true, false, false]);
-    } else if (e.nativeEvent.target === 13) {
-      setClicked([false, true, false]);
-    } else if (e.nativeEvent.target === 19) {
-      setClicked([false, false, true]);
-    }
-  };
+  const [nation, setNation] = useState(1);
 
   return (
     <View style={styles.container}>
       <View style={styles.buttonBox}>
         <SmallButton
           text="가요"
-          isClicked={clicked[0]}
-          setIsClicked={clickHandelr}
+          isClicked={nation === 1}
+          setIsClicked={() => {
+            setNation(1);
+          }}
         />
         <View style={styles.abroad}>
           <SmallButton
             text="POP"
-            isClicked={clicked[1]}
-            setIsClicked={clickHandelr}
+            isClicked={nation === 2}
+            setIsClicked={() => {
+              setNation(2);
+            }}
           />
           <SmallButton
             text="J-POP"
-            isClicked={clicked[2]}
-            setIsClicked={clickHandelr}
+            isClicked={nation === 3}
+            setIsClicked={() => {
+              setNation(3);
+            }}
           />
         </View>
       </View>
