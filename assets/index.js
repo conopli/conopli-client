ef8826033588de8cf938f3fa5b9a6cdb9e71f99e
nine_floor <less0805@gmail.com>
2023-05-01 16:24:44 +0900

    feat: 더미 데이터 업데이트 및 기존 assets에 있던 더미 제거

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,5 @@
 import google from './google.svg';
 import kakao from './kakao.svg';
 import naver from './naver.svg';
-import chart_dummy from './chart_dummy.js';
 
-export { google, kakao, naver, chart_dummy };
+export { google, kakao, naver };
