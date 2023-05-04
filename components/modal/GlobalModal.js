692de6929cbcb971765c229e2c29bce13c8aac41
Jade <neederjade@gmail.com>
2023-05-04 23:55:20 +0900

    feat: GlobalModal 컴포넌트 작성

---
+++ components/modal/GlobalModal.js
@@ -0,0 +1,42 @@
+import ModalState from '../../recoil/modal.js';
+import { useResetRecoilState, useRecoilValue } from 'recoil';
+import { Modal, TouchableOpacity } from 'react-native';
+import {
+  AddSongModal,
+  ConfirmModal,
+  DeleteModal,
+  EditPlaylistModal,
+  AddPlaylistModal,
+} from './index.js';
+import styles from './GlobalModal.style';
+
+const GlobalModal = () => {
+  const { isOpen, modalType, props } = useRecoilValue(ModalState);
+  const reset = useResetRecoilState(ModalState);
+
+  if (!isOpen) return;
+
+  const modal = {
+    addSong: <AddSongModal {...props} />,
+    confirm: <ConfirmModal {...props} />,
+    delete: <DeleteModal {...props} />,
+    editPlaylist: <EditPlaylistModal {...props} />,
+    addPlaylist: <AddPlaylistModal {...props} />,
+  };
+
+  return (
+    <Modal animationType="fade" transparent={isOpen} visible={isOpen}>
+      <TouchableOpacity
+        activeOpacity={1}
+        style={styles.backdrop}
+        onPress={() => {
+          reset();
+        }}
+      >
+        {modal[modalType]}
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+
+export default GlobalModal;
