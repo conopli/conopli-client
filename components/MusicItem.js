bf918ad590e77944329e8e1727168a552a024eb7
Jade <neederjade@gmail.com>
2023-05-05 22:32:16 +0900

    feat: MusicItem 컴포넌트에 AddSongModal 연결

--- components/MusicItem.js
+++ components/MusicItem.js
@@ -1,10 +1,34 @@
 import { Text, TouchableOpacity, View } from 'react-native';
 import styles from './MusicItem.style';
+import { useSetRecoilState } from 'recoil';
+import ModalState from '../recoil/modal.js';
 
 const MusicItem = ({ item }) => {
   const { num, singer, title } = item;
+  const setModal = useSetRecoilState(ModalState);
+
+  const songModal = {
+    isOpen: true,
+    modalType: 'addSong',
+    props: {
+      selectedSong: {
+        title,
+        singer,
+        num,
+      },
+      handler: () => {
+        console.log('addSong...!');
+      },
+    },
+  };
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => {
+        setModal(songModal);
+      }}
+    >
       <View style={styles.songInfo}>
         <Text style={styles.title} numberOfLines={1}>
           {title}
