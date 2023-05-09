ca59f48590f565e565554932e5b1c71078e1ae55
Jade <neederjade@gmail.com>
2023-05-09 23:07:39 +0900

    feat: 인기 차트서 곡 추가 모달 연결

--- components/popular/ListItem.js
+++ components/popular/ListItem.js
@@ -18,7 +18,22 @@ const ListItem = ({ item }) => {
         num,
       },
       handler: () => {
-        console.log('addSong...!');
+        //TODO:: 플레이리스트에 곡 추가하는 로직
+        setModal(confirmMove);
+      },
+    },
+  };
+
+  const confirmMove = {
+    isOpen: true,
+    modalType: 'confirm',
+    props: {
+      title: '추가가 완료되었습니다.',
+      subTitle: '플레이리스트로 이동할까요?',
+      buttonText: '이동',
+      handler: () => {
+        //TODO:: 추가한 플레이리스트로 이동하는 로직
+        console.log('go to playlist!');
       },
     },
   };
