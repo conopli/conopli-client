7bda33d5992f419154c5258244d740df76d0aae9
Jade <neederjade@gmail.com>
2023-05-10 23:08:49 +0900

    feat: 플레이리스트에 모달 연결 #10

--- components/Playlist/PlaylistItem.js
+++ components/Playlist/PlaylistItem.js
@@ -1,15 +1,57 @@
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';
 import { playlistColor } from '../../theme';
 import styles from './PlaylistItem.style';
+import { useSetRecoilState } from 'recoil';
+import ModalState from '../../recoil/modal.js';
 
 const PlaylistItem = ({ navigation, item }) => {
   const { color, emoji, title } = item;
+  const setModal = useSetRecoilState(ModalState);
+
+  const editModal = {
+    isOpen: true,
+    modalType: 'editPlaylist',
+    props: {
+      editHandler: () => {
+        setModal(editPlaylist);
+      },
+      deleteHandler: () => {
+        setModal(deletePlaylist);
+      },
+    },
+  };
+
+  const editPlaylist = {
+    isOpen: true,
+    modalType: 'addPlaylist',
+    props: {
+      title: '플레이리스트 수정',
+      buttonText: '수정완료',
+      oldName: title,
+      oldIcon: emoji,
+      oldColor: color.toString(),
+      handler: () => {},
+    },
+  };
+
+  const deletePlaylist = {
+    isOpen: true,
+    modalType: 'delete',
+    props: {
+      handler: () => {
+        console.log('delete playlist!');
+      },
+    },
+  };
 
   return (
     <TouchableOpacity
       style={[styles.container, { backgroundColor: playlistColor[color] }]}
       onPress={() => {
         navigation.push('Detail');
+      }}
+      onLongPress={() => {
+        setModal(editModal);
       }}
     >
       <View style={styles.iconContainer}>
--- screen/Playlist/Playlist.js
+++ screen/Playlist/Playlist.js
@@ -2,11 +2,39 @@ import { Text, View, FlatList } from 'react-native';
 import styles from './Playlist.style';
 import { PlaylistItem } from '../../components';
 import { playlistDummy } from '../../util';
+import { useEffect } from 'react';
+import { PlusButton } from '../../components/index';
+import { useSetRecoilState } from 'recoil';
+import ModalState from '../../recoil/modal.js';
 
 const Playlist = ({ navigation }) => {
+  const setModal = useSetRecoilState(ModalState);
+
+  const addPlaylist = {
+    isOpen: true,
+    modalType: 'addPlaylist',
+    props: {
+      title: '플레이리스트 추가',
+      buttonText: '추가하기',
+      buttonHandler: () => {
+        console.log('add playlist!');
+      },
+    },
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <PlusButton
+          buttonHandler={() => {
+            setModal(addPlaylist);
+          }}
+        />
+      ),
+    });
+  });
   return (
     <View style={styles.container}>
-      <Text>플레이리스트 홈</Text>
       <FlatList
         data={playlistDummy.data}
         renderItem={(props) => (
