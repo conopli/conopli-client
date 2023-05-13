88349e23195f810d9af414f8eef8aef6508fb62a
nine_floor <less0805@gmail.com>
2023-05-13 19:37:27 +0900

    feat: Map API 연결 #11

--- screen/Map.js
+++ screen/Map.js
@@ -1,12 +1,69 @@
-import { Text, View, Platform } from 'react-native';
-import { theme } from '../theme';
+import { View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import styles from './Map.style';
+import MapButton from '../components/Map/MapButton';
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
+import { useEffect, useState, useRef } from 'react';
+import getEnv from '../env';
+import { useSetRecoilState } from 'recoil';
+import ModalState from '../recoil/modal';
 
 const Map = () => {
+  const [hasPermission, setHasPermission] = useState(true);
+  const [location, setLocation] = useState({
+    latitude: 37.5662952,
+    longitude: 126.9779451,
+  });
+  const setModal = useSetRecoilState(ModalState);
+
+  useEffect(() => {
+    getPermission();
+    getLocation();
+  }, []);
+
+  const { BASE_URL } = getEnv();
+
+  const getPermission = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    console.log(granted);
+
+    if (!granted) {
+      setHasPermission(false);
+
+      const props = {
+        isOpen: true,
+        modalType: 'alert',
+        props: {
+          title: '위치 권한 오류',
+          subTitle: `현재 사용자의 위치를 확인할 수 없습니다.\n위치 권한을 사용할 수 있도록 승인해주세요.`,
+        },
+      };
+
+      setModal(props);
+    }
+  };
+
+  const getLocation = async () => {
+    const {
+      coords: { latitude, longitude },
+    } = await getCurrentPositionAsync({ accuracy: 5 });
+
+    setLocation({ latitude, longitude });
+  };
+
   return (
-    <WebView
-      source={{ uri: 'https://map.kakao.com/?urlX=523954.0&urlY=1084098.0' }}
-    />
+    <View style={styles.container}>
+      <WebView
+        source={{
+          uri: `${BASE_URL}/maps?searchType=코인노래&lng=${location.longitude}&lat=${location.latitude}`,
+        }}
+      />
+      <MapButton handler={getLocation} />
+    </View>
   );
 };
 
--- screen/Map.style.js
+++ screen/Map.style.js
@@ -6,6 +6,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.background,
     justifyContent: 'center',
+    position: 'relative',
   },
 });
 
