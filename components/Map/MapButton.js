e704985e0d7477c8341c1287fd6477af4e35959f
nine_floor <less0805@gmail.com>
2023-05-13 19:30:46 +0900

    fix: 오타 및 스타일 오류 수정 #11

--- components/Map/MapButton.js
+++ components/Map/MapButton.js
@@ -1,13 +1,11 @@
-import { Text, TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import styles from './MapButton.style';
 import { mapIcon } from '../../assets/index.js';
 import { WithLocalSvg } from 'react-native-svg';
 
 const MapButton = ({ handler }) => {
-  //TODO::
-  //handler에 위치 권한 동의 로직 받아와서 새로운 요청 보내도록
   return (
-    <TouchableOpacity style={styles.mapButton} onPerss={handler}>
+    <TouchableOpacity style={styles.mapButton} onPress={handler}>
       <WithLocalSvg width={32} height={32} asset={mapIcon} />
     </TouchableOpacity>
   );
