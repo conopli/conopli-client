bb4b8a337859fc58c275b7ba4bd2d89f6eff393b
Jade <neederjade@gmail.com>
2023-05-21 19:14:57 +0900

    feat: 리코일 atom으로 userPlayList 추가 #12

---
+++ recoil/userPlayList.js
@@ -0,0 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { atom } from 'recoil';
+
+const asyncStorageEffect =
+  (key) =>
+  async ({ setSelf, onSet, trigger }) => {
+    const loadPersisted = async () => {
+      const savedValue = await AsyncStorage.getItem(key);
+
+      if (savedValue != null) {
+        setSelf(JSON.parse(savedValue));
+      }
+    };
+
+    if (trigger === 'get') loadPersisted();
+
+    onSet(async (newValue, _, isReset) => {
+      if (isReset) {
+        await AsyncStorage.removeItem(key);
+      } else {
+        await AsyncStorage.setItem(key, JSON.stringify(newValue));
+      }
+    });
+  };
+
+const userPlayList = atom({
+  key: 'userPlayList',
+  default: {
+    playlist: [],
+  },
+  effects: [asyncStorageEffect('user_playList')],
+});
+
+export default userPlayList;
