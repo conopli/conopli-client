d41a8aa229f8b900b2d659db73a5444e3b4234c2
Jade <neederjade@gmail.com>
2023-05-29 08:25:16 +0900

    Feat: addPlayList form validation 함수 추가 #12

--- screen/Playlist/PlaylistModify.js
+++ screen/Playlist/PlaylistModify.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { View, ScrollView, FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import styles from './PlaylistModify.style';
 import {
   BackButton,
@@ -9,10 +9,20 @@ import {
 } from '../../components/Playlist';
 import { detailDummy } from '../../util';
 
+//TODO:: 플레이리스트 내부 수정
+
 const PlaylistModify = ({ navigation }) => {
+  //플리 내부에 있는 노래 리스트
   const [items, setItems] = useState(detailDummy.data);
+  //삭제를 위한 기능 : 선택한 노래들을 배열에 추가 (userMusicId string이 담김);
   const [select, setSelect] = useState([]);
+  //요 상태 기준으로 반복문 돌려서 delete 요청 보내야 함,,,
   const [deleteItems, setDeleteItems] = useState([]);
+
+  //플레이리스트 이동 같은 경우 add, delete // confirmModal으로 확인
+  //플레이리스트 내부에서 수정의 경우 연필 버튼 > 플리 이동 시 바로 적용
+  //플레이리스트 내부에서 삭제의 경우 삭제할 노래들 선택 > 체크버튼 눌러야 적용되고, 만약 뒤로가기 누르면 적용 x
+  //두 로직 간 차이점이 있음 - 유저의 혼란 가능성 있음
 
   useEffect(() => {
     navigation.setOptions({
