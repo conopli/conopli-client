f285d40eaea79b92a385f21800d2248350945edf
nine_floor <less0805@gmail.com>
2023-05-30 20:50:15 +0900

    feat: 로고 파일 추가

--- assets/index.js
+++ assets/index.js
@@ -2,5 +2,7 @@ import google from './google.svg';
 import kakao from './kakao.svg';
 import naver from './naver.svg';
 import mapIcon from './mapIcon.svg';
+import symbolLogo from './symbolLogo.svg';
+import typoLogo from './typoLogo.svg';
 
-export { google, kakao, naver, mapIcon };
+export { google, kakao, naver, mapIcon, symbolLogo, typoLogo };
