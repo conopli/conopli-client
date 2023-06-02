4786f2d8d6e4faf1ff4ebd638521646400119f7e
nine_floor <less0805@gmail.com>
2023-06-02 18:43:17 +0900

    remove: 필요없는 파일 삭제

--- assets/index.js
+++
@@ -1,8 +0,0 @@
-import google from './google.svg';
-import kakao from './kakao.svg';
-import naver from './naver.svg';
-import mapIcon from './mapIcon.svg';
-import symbolLogo from './symbolLogo.svg';
-import typoLogo from './typoLogo.svg';
-
-export { google, kakao, naver, mapIcon, symbolLogo, typoLogo };
