64a79dc81dca915a009293b341e1e9dadbc54111
Jade <neederjade@gmail.com>
2023-08-10 20:07:48 +0900

    feat: 회원 탈퇴기능 로그인 한 유저에게만 노출 및 문의하기 버튼 토스트 처리

--- components/modal/SettingModal.js
+++ components/modal/SettingModal.js
@@ -1,12 +1,17 @@
 import { Text, View } from 'react-native';
 import styles from './EditPlaylistModal.style.js';
 import { RowButton } from '../index.js';
-import { useResetRecoilState } from 'recoil';
+import { useResetRecoilState, useRecoilValue } from 'recoil';
+import userInfo from '../../recoil/userInfo';
 import ModalState from '../../recoil/modal.js';
+import { makeToast } from '../../util';
 
 //설정 페이지 우측 상단 버튼 클릭 시
 
 const EditPlaylistModal = ({ leaveHandler }) => {
+  const { userId } = useRecoilValue(userInfo);
+  const reset = useResetRecoilState(ModalState);
+
   return (
     <View
       style={styles.modalContainer}
@@ -17,21 +22,33 @@ const EditPlaylistModal = ({ leaveHandler }) => {
     >
       <Text style={styles.title}>설정 더보기</Text>
       <View style={styles.buttonBox}>
-        <View style={styles.buttonItem}>
-          <RowButton
-            text="탈퇴하기"
-            color="red"
-            buttonHandler={() => {
-              leaveHandler();
-            }}
-          />
-        </View>
+        {userId !== 0 && (
+          <View style={styles.buttonItem}>
+            <RowButton
+              text="탈퇴하기"
+              color="red"
+              buttonHandler={() => {
+                leaveHandler();
+              }}
+            />
+          </View>
+        )}
         <View style={styles.buttonItem}>
           <RowButton
             text="문의하기"
             color="lime"
             buttonHandler={() => {
-              console.log('문의하기는 준비중!');
+              reset();
+              makeToast('준비중인 기능입니다!');
+            }}
+          />
+        </View>
+        <View style={styles.buttonItem}>
+          <RowButton
+            text="취소"
+            color="gray"
+            buttonHandler={() => {
+              reset();
             }}
           />
         </View>
