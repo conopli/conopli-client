32c5514b25d9b7909a540c24026f3757b2c0142a
nine_floor <less0805@gmail.com>
2023-08-15 19:37:28 +0900

    feat: 상단 바 적용

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import Home from './screen/Home';
 import { RecoilRoot } from 'recoil';
 import { RootSiblingParent } from 'react-native-root-siblings';
 import 'react-native-url-polyfill/auto';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <RootSiblingParent>
       <RecoilRoot>
+        <StatusBar style="auto" />
         <Home />
       </RecoilRoot>
     </RootSiblingParent>
