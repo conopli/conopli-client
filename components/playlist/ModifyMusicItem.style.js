e2c3646e913a5554fee5d62951d18c67028eb721
nine_floor <less0805@gmail.com>
2023-09-10 21:07:37 +0900

    fix: 폴더 대문자 => 소문자 변경

---
+++ components/playlist/ModifyMusicItem.style.js
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    flex: 1,
+    backgroundColor: theme.white,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    columnGap: 12,
+    borderRadius: 4,
+  },
+  select: {
+    backgroundColor: theme.lightBlue,
+  },
+  songInfo: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    rowGap: 4,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  artist: {
+    fontSize: 16,
+  },
+  numberBox: {
+    width: 60,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  number: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  },
+});
+
+export default styles;
