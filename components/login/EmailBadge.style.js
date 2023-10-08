4405bee295b7622b8cfbbd2c8f24946ff7a4f799
Jade <neederjade@gmail.com>
2023-10-08 16:24:20 +0900

    feat: EmailBadge 컴포넌트 생성 #31

---
+++ components/login/EmailBadge.style.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../theme';
+
+const styles = StyleSheet.create({
+  emailInfo: {
+    alignItems: 'center',
+    marginBottom: 48,
+    rowGap: 8,
+  },
+  email: {
+    flexDirection: 'row',
+    padding: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  KAKAO: {
+    backgroundColor: theme.kakao,
+  },
+  GOOGLE: {
+    backgroundColor: theme.white,
+  },
+  NAVER: {
+    backgroundColor: theme.naver,
+  },
+  emailText: {
+    fontSize: 12,
+    color: theme.black,
+    marginRight: 8,
+  },
+});
+
+export default styles;
