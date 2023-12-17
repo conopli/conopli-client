176bb915b332e22a2733b84bec7a587cabf9096d
nine_floor <less0805@gmail.com>
2023-12-17 16:55:42 +0900

    feat: 서버 버전 체크 API 연결 및 로직 일부 수정(비동기 동작)

--- App.js
+++ App.js
@@ -8,79 +8,66 @@ import * as Splash from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
 import 'react-native-gesture-handler';
 import axios from 'axios';
-import { Alert, BackHandler, Linking } from 'react-native';
+import versionAlert from './util/versionAlert';
 
 Splash.preventAutoHideAsync();
 
 export default function App() {
-  const appVersion = '0.2';
-  const [isServerReady, setIsServerReady] = useState(false);
-  const [isFontLoading, setIsFontLoading] = useState(false);
+  const availableVersion = '0.2';
   const [isHideSplash, setIsHideSplash] = useState(false);
 
   const fetchFonts = async () => {
-    await Font.loadAsync({
-      Pretendard: require('./assets/fonts/PretendardJPVariable.ttf'),
-      'Pretendard-100': require('./assets/fonts/PretendardJP-Thin.otf'),
-      'Pretendard-200': require('./assets/fonts/PretendardJP-ExtraLight.otf'),
-      'Pretendard-300': require('./assets/fonts/PretendardJP-Light.otf'),
-      'Pretendard-400': require('./assets/fonts/PretendardJP-Regular.otf'),
-      'Pretendard-500': require('./assets/fonts/PretendardJP-Medium.otf'),
-      'Pretendard-600': require('./assets/fonts/PretendardJP-SemiBold.otf'),
-      'Pretendard-700': require('./assets/fonts/PretendardJP-Bold.otf'),
-      'Pretendard-800': require('./assets/fonts/PretendardJP-ExtraBold.otf'),
-      'Pretendard-900': require('./assets/fonts/PretendardJP-Black.otf'),
-    });
-    setIsFontLoading(true);
+    try {
+      await Font.loadAsync({
+        Pretendard: require('./assets/fonts/PretendardJPVariable.ttf'),
+        'Pretendard-100': require('./assets/fonts/PretendardJP-Thin.otf'),
+        'Pretendard-200': require('./assets/fonts/PretendardJP-ExtraLight.otf'),
+        'Pretendard-300': require('./assets/fonts/PretendardJP-Light.otf'),
+        'Pretendard-400': require('./assets/fonts/PretendardJP-Regular.otf'),
+        'Pretendard-500': require('./assets/fonts/PretendardJP-Medium.otf'),
+        'Pretendard-600': require('./assets/fonts/PretendardJP-SemiBold.otf'),
+        'Pretendard-700': require('./assets/fonts/PretendardJP-Bold.otf'),
+        'Pretendard-800': require('./assets/fonts/PretendardJP-ExtraBold.otf'),
+        'Pretendard-900': require('./assets/fonts/PretendardJP-Black.otf'),
+      });
+      setIsHideSplash(true);
+      setTimeout(() => {
+        Splash.hideAsync();
+      }, 200);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const getServerStatus = async () => {
-    // TODO : API 업데이트 이후 주소 및 데이터 다루는거 수정
     try {
       const {
         data: {
           data: { version: serverVersion },
         },
-      } = await axios.get(`${process.env.EXPO_PUBLIC_BASE_URL}/`);
-      setIsServerReady(serverVersion === appVersion);
+      } = await axios.get(`${process.env.EXPO_PUBLIC_BASE_URL}/version`);
+      console.log('serverVersion:', serverVersion);
+
+      if (serverVersion !== availableVersion) {
+        versionAlert();
+        return false;
+      } else return true;
     } catch (err) {
       console.log(err);
     }
   };
 
   useEffect(() => {
-    if (!isServerReady) {
-      Alert.alert(
-        '필수 업데이트 안내',
-        `새로운 버전이 업데이트 되었습니다.\n확인을 누르시면 스토어로 이동합니다.\n(업데이트를 하지 않으면 앱을 이용할 수 없습니다.)`,
-        [
-          {
-            text: '확인',
-            onPress: () => {
-              Linking.openURL('market://details?id=com.conopli');
-              BackHandler.exitApp();
-            },
-          },
-          {
-            text: '취소',
-            onPress: () => {
-              BackHandler.exitApp();
-            },
-            style: 'cancle',
-          },
-        ],
-      );
-      return;
-    }
+    const checkVersion = async () => {
+      try {
+        const isServerReady = await getServerStatus();
+        if (isServerReady) await fetchFonts();
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    fetchFonts();
-
-    if (isFontLoading) {
-      setIsHideSplash(true);
-      setTimeout(() => {
-        Splash.hideAsync();
-      }, 200);
-    }
+    checkVersion();
   }, []);
 
   return (
